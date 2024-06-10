48fc9e6ccfda0187ac5e3111ff58a6f3d7207cec
Prabin Rag TK <prabin.rag@claysys.in>
2024-06-10 22:11:27 +0530

    ConsoleAppInit

---
+++ ConsoleApp06102024/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp06102024
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
