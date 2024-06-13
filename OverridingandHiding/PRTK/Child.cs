73626d4e2a8352a6ffb7109b145b83e9bc04f08d
Prabin Rag TK <prabin.rag@claysys.in>
2024-06-13 22:13:02 +0530

    Override and hiding

---
+++ OverridingandHiding/PRTK/Child.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace OverridingandHiding.PRTK
+{
+    public class Child : Parent
+    {
+        public override void ParentMethod1()
+        {
+            Console.WriteLine("Child Method 1");
+        }
+        public new void ParentMethod2()
+        {
+            Console.WriteLine("Child Method 2");
+        }
+        public void ParentCall()
+        {
+            base.ParentMethod1 ();
+            base.ParentMethod2 ();
+        }
+        static void Main()
+        {
+            Child child = new Child();
+            Parent parent = child;
+            child.ParentMethod1();
+            child.ParentMethod2();
+            parent.ParentMethod1();
+            parent.ParentMethod2();
+            child.ParentCall();
+            Console.ReadLine();
+        }
+    }
+}
---
+++ OverridingandHiding/PRTK/Parent.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OverridingandHiding.PRTK
+{
+    public class Parent
+    {
+        public virtual void ParentMethod1()
+        {
+            Console.WriteLine("Parent Method 1");
+        }
+        public void ParentMethod2() 
+        {
+            Console.WriteLine("Parent Method 2");
+        }
+    }
+}
